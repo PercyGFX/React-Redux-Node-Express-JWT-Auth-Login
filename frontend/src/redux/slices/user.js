40f977a291983a96881d96e3f202a6078bba398e
PercyGFX <isurangabtk@gmail.com>
2023-10-02 22:56:38 +0530

    comments

--- frontend/src/redux/slices/user.js
+++ frontend/src/redux/slices/user.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import Cookies from "js-cookie";
 import jwtDecode from "jwt-decode";
 
+// get token cookie and decode if there is an one
 const token = Cookies.get("token");
 let decodedToken = null;
 
@@ -13,8 +14,11 @@ if (token) {
   }
 }
 
+// crate slice
 export const userSlice = createSlice({
   name: "user",
+
+  // set intnitial states if there is a token. otherwise null and false. keep isauth false. it will be handled from saga
   initialState: {
     username: decodedToken?.username || null,
     isLogged: decodedToken ? true : false,
@@ -22,16 +26,20 @@ export const userSlice = createSlice({
   },
 
   reducers: {
+    // this is the reducer for saga API check
     authcheck: (state, action) => {
       state.username = action.payload.username;
       state.isLogged = action.payload.isAuthenticated;
       state.isAuthenticated = action.payload.isAuthenticated;
     },
+
+    // reducer for manually set when user login
     login: (state, action) => {
       state.username = action.payload.username;
       state.isLogged = action.payload.isAuthenticated;
       state.isAuthenticated = action.payload.isAuthenticated;
     },
+    // logout reducer
     logout: (state) => {
       state.username = null;
       state.isLogged = false;
